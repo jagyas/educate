a521876e8d7adb53e9058193dd58d0d2ccbbc4de
Jagyas <admin@dig2logic.com>
2019-03-05 16:32:16 +0000

    Javascript integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Subscription } from 'rxjs';
+
+declare var browser:any;
+declare var breakpoints:any;
+import '../assets/js/breakpoints.min.js';
+import '../assets/js/browser.min.js';
+import '../assets/js/util.js';
+
+declare var $:any;
 
 @Component({
   selector: 'app-educator',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {}
+export class AppComponent implements OnInit {
+    
+    ngOnInit() {
+
+    }
+}
