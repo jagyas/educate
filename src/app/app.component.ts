e90aa1952a2fd8c32a619722044435ccde3d611d
Jagyas <admin@dig2logic.com>
2019-03-06 08:10:32 +0000

    Minor change in JS file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 
 declare var browser:any;
 declare var breakpoints:any;
+declare var navigator:any;
 import '../assets/js/breakpoints.min.js';
 import '../assets/js/browser.min.js';
 import '../assets/js/util.js';
