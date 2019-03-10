06311d9eee30e012ac298d854d45574b5d986740
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-10 21:18:48 +0530

    Pointed renderer to head

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -13,18 +13,18 @@ export class HomepageComponent implements AfterViewInit {
         console.log('AfterViewInit');
         let s = this._renderer2.createElement('script');
             s.src = `assets/js/jquery.min.js`;
-            this._renderer2.appendChild(this._document.div, s);
+            this._renderer2.appendChild(this._document.head, s);
         let s1 = this._renderer2.createElement('script');
             s1.src = `assets/js/browser.min.js`;
-            this._renderer2.appendChild(this._document.div, s1);
+            this._renderer2.appendChild(this._document.head, s1);
         let s2 = this._renderer2.createElement('script');
             s2.src = `assets/js/breakpoints.min.js`;
-            this._renderer2.appendChild(this._document.div, s2);
+            this._renderer2.appendChild(this._document.head, s2);
         let s3 = this._renderer2.createElement('script');
             s3.src = `assets/js/util.js`;
-            this._renderer2.appendChild(this._document.div, s3);
+            this._renderer2.appendChild(this._document.head, s3);
         let s4 = this._renderer2.createElement('script');
             s4.src = `assets/js/main1.js`;
-            this._renderer2.appendChild(this._document.div, s4);
+            this._renderer2.appendChild(this._document.head, s4);
     }
 }
