84e0db0c2017ff765ef4c4ea04463b8efb91a168
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-15 06:14:10 +0530

    Refactoring html code

--- src/app/stories/stories-routing.module.ts
+++ src/app/stories/stories-routing.module.ts
@@ -5,6 +5,7 @@ import { StoriesComponent } from './stories.component';
 import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
 
 const routes: Routes = [
+    { path: 'posts', component: StoriesComponent, pathMatch: 'full'},
     { path: ':id', component: StoriesComponent, pathMatch: 'full'}
 ];
 
