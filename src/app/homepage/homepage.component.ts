4d857f1766f2614c36e88b73029db12acd744222
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-16 10:40:45 +0530

    Javascript Libraries load

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,24 +7,21 @@ import { DOCUMENT } from '@angular/platform-browser';
   styles: []
 })
 export class HomepageComponent implements AfterViewInit {
-    constructor(private _renderer2: Renderer2, @Inject(DOCUMENT) private _document) { }
-    // @ViewChild('content') content;
-    ngAfterViewInit() {
-        console.log('AfterViewInit');
-        let s = this._renderer2.createElement('script');
-            s.src = `assets/js/jquery.min.js`;
-            this._renderer2.appendChild(this._document.head, s);
-        let s1 = this._renderer2.createElement('script');
-            s1.src = `assets/js/browser.min.js`;
-            this._renderer2.appendChild(this._document.head, s1);
-        let s2 = this._renderer2.createElement('script');
-            s2.src = `assets/js/breakpoints.min.js`;
-            this._renderer2.appendChild(this._document.head, s2);
-        let s3 = this._renderer2.createElement('script');
-            s3.src = `assets/js/util.js`;
-            this._renderer2.appendChild(this._document.head, s3);
-        let s4 = this._renderer2.createElement('script');
-            s4.src = `assets/js/main1.js`;
-            this._renderer2.appendChild(this._document.head, s4);
-    }
+  constructor(private _renderer2: Renderer2, @Inject(DOCUMENT) private _document) { }
+  async ngAfterViewInit() {
+    await this.loadScript("assets/js/jquery.min.js");
+    await this.loadScript("assets/js/browser.min.js");
+    await this.loadScript("assets/js/breakpoints.min.js");
+    await this.loadScript("assets/js/util.js");
+    await this.loadScript("assets/js/main1.js");
+  }
+
+  private loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = this._document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      this._document.body.appendChild(scriptElement);
+    });
+  }
 }
