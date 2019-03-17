3f824d1bf8025ca2891389f1168cde3925d86960
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-17 09:17:24 +0530

    Markdown major changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-
+import { homepageRoutesNames } from './homepage/homepage.routes.names';
 
 @Component({
   selector: 'app-educator',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  postsLink = `./${homepageRoutesNames.POSTS}`;
+}
