b02b3d447792272488d99a7261cfc0e18acbdabb
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-17 12:08:23 +0530

    JS files universal load fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Inject, AfterViewInit  } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+
 import { homepageRoutesNames } from './homepage/homepage.routes.names';
 
 @Component({
@@ -6,6 +8,23 @@ import { homepageRoutesNames } from './homepage/homepage.routes.names';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   postsLink = `./${homepageRoutesNames.POSTS}`;
+  constructor(@Inject(DOCUMENT) private _document) { }
+  async ngAfterViewInit() {
+    await this.loadScript('assets/js/jquery.min.js');
+    await this.loadScript('assets/js/browser.min.js');
+    await this.loadScript('assets/js/breakpoints.min.js');
+    await this.loadScript('assets/js/util.js');
+    await this.loadScript('assets/js/main1.js');
+  }
+
+  private loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = this._document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      this._document.body.appendChild(scriptElement);
+    });
+  }
 }
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,27 +1,8 @@
-import { Component, Inject, AfterViewInit } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styles: []
 })
-export class HomepageComponent implements AfterViewInit {
-  constructor(@Inject(DOCUMENT) private _document) { }
-  async ngAfterViewInit() {
-    await this.loadScript('assets/js/jquery.min.js');
-    await this.loadScript('assets/js/browser.min.js');
-    await this.loadScript('assets/js/breakpoints.min.js');
-    await this.loadScript('assets/js/util.js');
-    await this.loadScript('assets/js/main1.js');
-  }
-
-  private loadScript(scriptUrl: string) {
-    return new Promise((resolve, reject) => {
-      const scriptElement = this._document.createElement('script');
-      scriptElement.src = scriptUrl;
-      scriptElement.onload = resolve;
-      this._document.body.appendChild(scriptElement);
-    });
-  }
-}
+export class HomepageComponent {}
