caa115c70dea6444d3ceee317da3728885d494ca
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-19 21:17:13 +0530

    Custom tag return from markdown

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { MarkdownService } from 'ngx-markdown';
 
 @Component({
   selector: 'app-stories',
@@ -11,12 +12,20 @@ export class StoriesComponent implements OnInit, OnDestroy {
 
  private sub: Subscription;
  post: string;
- constructor(private route: ActivatedRoute) { }
+ constructor(private route: ActivatedRoute, private markdownService: MarkdownService) { }
 
  ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.post = './assets/blog/posts/' +  params['id'] + '.md';
     });
+    this.markdownService.renderer.heading = (text: string, level: number) => {
+      const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+      return '<header class="main">' + '<h' + level + '>' + text + '</h' + level + '>' + '</header>';
+    };
+    this.markdownService.renderer.image = (href: string, title: string, text: string) => {
+      const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+      return '<span class="image main"> <img src="' + href + '" title="' + title + '" alt="' + text + '" /> </span>';
+    };
  }
 
  ngOnDestroy() {
