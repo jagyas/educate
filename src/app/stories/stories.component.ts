0c307432890d9419db55b1899ecb69d3222055ed
Jagdeep Singh <jagdeep.singh044@gmail.com>
2019-03-20 22:13:45 +0530

    Major Success

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -12,11 +12,12 @@ export class StoriesComponent implements OnInit, OnDestroy {
 
  private sub: Subscription;
  post: string;
+ postdata: any;
  constructor(private route: ActivatedRoute, private markdownService: MarkdownService) { }
 
  ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      this.post = './assets/blog/posts/' +  params['id'] + '.md';
+      this.post = '../../assets/blog/posts/' +  params['id'] + '.md';
     });
     this.markdownService.renderer.heading = (text: string, level: number) => {
       const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
@@ -26,6 +27,17 @@ export class StoriesComponent implements OnInit, OnDestroy {
       const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
       return '<span class="image main"> <img src="' + href + '" title="' + title + '" alt="' + text + '" /> </span>';
     };
+    this.markdownService.getSource('../../assets/blog/posts/test3.md').subscribe(function (data: string) {
+      
+      let x = data.split('---').slice(2, 3);
+      this.postdata = x[0];
+      console.log('DEBUG: RouteEvent Log area seven' + x[0]);
+    }.bind(this), function (errors) {
+
+      console.log('DEBUG:E: RouteEvent Log area eight', errors);
+      this.postdata = 'Error';
+
+    }.bind(this));
  }
 
  ngOnDestroy() {
